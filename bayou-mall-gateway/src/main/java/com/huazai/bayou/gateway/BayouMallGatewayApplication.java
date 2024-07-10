e1463245e685436a8d96e60ca1415c26294c00b1
huazai <1461522031@qq.com>
2024-07-10 11:56:34 +0800

    新增网关，项目

--- bayou-mall-gateway/src/main/java/com/huazai/bayou/gateway/BayouMallGatewayApplication.java
+++ bayou-mall-gateway/src/main/java/com/huazai/bayou/gateway/BayouMallGatewayApplication.java
@@ -2,11 +2,12 @@ package com.huazai.bayou.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- *
  * @author Jackson
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BayouMallGatewayApplication {
 
