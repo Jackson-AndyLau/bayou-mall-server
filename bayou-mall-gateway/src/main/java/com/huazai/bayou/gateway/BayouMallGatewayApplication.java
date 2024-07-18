511cb6ba5b840285672b160f65c70aece3744394
huazai <1461522031@qq.com>
2024-07-18 15:51:50 +0800

    解决Nacos与Spring Boot的版本冲突

--- bayou-mall-gateway/src/main/java/com/huazai/bayou/gateway/BayouMallGatewayApplication.java
+++ bayou-mall-gateway/src/main/java/com/huazai/bayou/gateway/BayouMallGatewayApplication.java
@@ -1,6 +1,7 @@
 package com.huazai.bayou.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -12,6 +13,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class BayouMallGatewayApplication {
 
     public static void main(String[] args) {
+
+        // 关闭Nacos日志
+//        System.setProperty("nacos.logging.default.config.enable","false");
         SpringApplication.run(BayouMallGatewayApplication.class, args);
     }
 
