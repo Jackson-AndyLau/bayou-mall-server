182ea08fffef394a30ce3df9a15b02b4ea2ca407
huazai <1461522031@qq.com>
2024-09-03 15:09:16 +0800

    清理secret

--- bayou-mall-product/src/test/java/com/huazai/bayou/product/BayouMallProductTests.java
+++ bayou-mall-product/src/test/java/com/huazai/bayou/product/BayouMallProductTests.java
@@ -32,9 +32,9 @@ public class BayouMallProductTests {
     void uploadFile() throws ClientException, FileNotFoundException {
         CredentialsProvider credentialsProvider01 = new CredentialsProvider() {
             // 初始化变量
-            String accessKeyId = "LTAI5t7bxvcBX9zbbqnXrHj2";
+            String accessKeyId = "";
             // 初始化变量
-            String accessKeySecrect = "t2jX1DYieiQzoJ4uyac9CgMo2VcvC5";
+            String accessKeySecrect = "";
 
             // 初始化变量
             // String token = null;
@@ -53,7 +53,7 @@ public class BayouMallProductTests {
             }
         };
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-chengdu.aliyuncs.com";
+        String endpoint = "";
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
         // EnvironmentVariableCredentialsProvider credentialsProvider02 = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
         // 填写Bucket名称，例如examplebucket。
--- bayou-mall-ware/src/test/java/com/huazai/bayou/ware/BaiyouMallWareApplicationTests.java
+++ bayou-mall-ware/src/test/java/com/huazai/bayou/ware/BaiyouMallWareApplicationTests.java
@@ -1,7 +1,5 @@
 package com.huazai.bayou.ware;
 
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.model.PutObjectResult;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.huazai.bayou.ware.entity.WareInfoEntity;
 import com.huazai.bayou.ware.service.WareInfoService;
@@ -9,9 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -19,9 +14,6 @@ class BaiyouMallWareApplicationTests {
 
     @Autowired
     private WareInfoService wareInfoService;
-
-    @Autowired
-    private OSSClient ossClient;
 
     @Test
     void contextLoads() {
@@ -40,21 +32,6 @@ class BaiyouMallWareApplicationTests {
             System.out.println(wareInfoEntity.toString());
 
         });
-    }
-
-    @Test
-    public void aliUploadFile() throws FileNotFoundException {
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "bayou-bucket";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = "product/my-product-04.png";
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-        String filePath = "F:\\图片1.png";
-        InputStream inputStream = new FileInputStream(filePath);
-
-        PutObjectResult putObjectResult = ossClient.putObject(bucketName, objectName, inputStream);
-
-        System.out.println("文件上传完成。。。。。。");
     }
 
 
